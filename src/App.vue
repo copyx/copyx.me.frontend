a044ff40f903f7e485641cd973d2b7fbf267eda0
Jingi Kim <copyx00@gmail.com>
2019-06-27 23:28:59 +0900

    feat: 백엔드에 HTTP 요청하기 위한 Axios 추가

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button v-on:click="requestTestToExpress">Express 요청 테스트</button>
   </div>
 </template>
 
@@ -12,6 +13,12 @@ export default {
   name: 'app',
   components: {
     HelloWorld
+  },
+  methods: {
+    requestTestToExpress: async function () {
+      const res = await this.$http.get('/');
+      console.log(res.data);
+    }
   }
 }
 </script>
